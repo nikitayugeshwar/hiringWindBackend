f8843a2e32a8b1acf16d2303184d2a78d96f28f1
nikitayugeshwar <nikitayugeshwar01@gmail.com>
2026-02-23 21:44:36 +0530

    vercel setup

--- src/app.js
+++ src/app.js
@@ -15,9 +15,14 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.get("/", (req, res) => {
+  res.send("backend is live");
+});
 app.use("/api/user", userRoute);
 app.use("/api/interview", interviewRoute);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server running on port ${process.env.PORT}`);
+// });
+
+module.exports = app;
