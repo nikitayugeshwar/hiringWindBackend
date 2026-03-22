a3d90bb5e027eae834c6d37ab3712b1bf70e3859
nikitayugeshwar <nikitayugeshwar01@gmail.com>
2026-03-22 21:54:30 +0530

    backend security fixed

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -36,7 +36,7 @@ exports.login = async (req, res) => {
     });
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "none",
+      sameSite: "lax",
       secure: false,
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
